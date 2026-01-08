3e61f9f974e1070138cd73ba4044f51d78396273
Khalilov Kurbon <khalilovbobo7@gmail.com>
2026-01-08 14:56:35 +0300

    сервер

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key")
 
-DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
+DEBUG = "True"
 
 ALLOWED_HOSTS = [
     "localhost",
